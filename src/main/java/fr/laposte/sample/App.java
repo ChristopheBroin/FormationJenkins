5f2ad43b86427a136f0ad0acd2b14fed1ab0e2fb
Christophe Broin <christophe.broin@laposte.fr>
2018-10-09 10:22:04 +0200

    first commit

---
+++ src/main/java/fr/laposte/sample/App.java
@@ -0,0 +1,13 @@
+package fr.laposte.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
